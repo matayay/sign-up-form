af02cfe45532f60843aa159aa33d52b674114d0e
Matayay Karuna <matayay2@gmail.com>
2023-01-03 14:45:35 -0700

    fixed a bug with confirm password

--- confirmPassword.js
+++ confirmPassword.js
@@ -4,19 +4,14 @@ button.addEventListener('mouseover', () => {
     const password = document.getElementById('user_password');
     const confirm_password = document.getElementById('confirm_password');
     const pass_div = document.getElementById('pass');
+    const not_same = pass_div.querySelector('p');
 
     if ( password.value != confirm_password.value )
     {
         password.value = "";
         confirm_password.value = "";
 
-        const msg = document.createElement('p');
-        msg.textContent = "*Passwords do not match.";
-        msg.style.color = "red";
-        msg.style.margin = "0rem";
-        msg.style.fontSize = "0.8rem"
-        pass_div.appendChild(msg);
-
+        not_same.textContent = "*Passwords do not match.";
         password.style.border = "2px solid red";
         confirm_password.style.border = "2px solid red";
     }
